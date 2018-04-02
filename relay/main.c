9f2ed36c7e1664d4b1bde64725d90b8d2c294811
suzhou.li@duke.edu <suzhou.li@duke.edu>
2018-04-02 17:21:56 -0400

    Wrote code to send data from the PIC to the PC through the serial port.

--- relay/main.c
+++ relay/main.c
@@ -16,7 +16,9 @@
 /******************************************************************************/
 #pragma interrupt interruptFunction
 void interruptFunction() {
-	LATBbits.LATB0 = !LATBbits.LATB0;
+	//LATBbits.LATB0 = !LATBbits.LATB0;
+    //Serial_ISR();
+    Serial_TX_REGISTER = 'b';
 }
 
 /******************************************************************************/
@@ -24,6 +26,7 @@ void interruptFunction() {
 /******************************************************************************/
 void main() {
 	unsigned char status, i;
+    unsigned char write[6] = {'a', 'b', 'c', 'd', 0x00, 0x00};
 	
 	/* Set the PIC clock frequency */
     OSCCON = 0b01110110; // set clock to 16 MHz
@@ -44,10 +47,14 @@ void main() {
 	/* Run code indefinitely */
 	if (status) {
 		while (1) {
-			Serial_TX_WriteBufferMultiple({0x02, 0x04, 0x08, 0x09});
-			for (i = 0; i < 4; i = i + 1) {
-				Serial_TX_SendByte();
-			}
+            //Serial_INTEN_TX = 1;
+            while(Serial_TX_EMPTY == 1) {
+                Serial_TX_REGISTER = 'a';
+            }
+            //Serial_TX_WriteBufferMultiple(write);
+			//for (i = 0; i < 4; i = i + 1) {
+			//	Serial_TX_SendByte();
+			//}
 		}
 	}
 }
